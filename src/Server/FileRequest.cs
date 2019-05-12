c0ebfb7677f735a6abdf078277b0702851dbd37c
Rapzy <andrushko.egor@gmail.com>
2019-05-12 23:43:07 +0300

    Create requests class

---
+++ src/Server/FileRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    [Serializable]
+    public class Request
+    {
+        public string RequestType { get; set; }
+        public Request (string type)
+        {
+            RequestType = type;
+        }
+    }
+    [Serializable]
+    public class RequestFile:Request
+    {
+        public int Offset { get; set; }
+        public int Length { get; set; }
+        public string FileName { get; set; }
+        public RequestFile(string filename, int offset, int length):
+            base("RequestFile")
+        {
+            FileName = filename;
+            Offset = offset;
+            Length = length;
+        }
+
+    }
+}
